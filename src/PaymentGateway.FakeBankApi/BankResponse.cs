deebf59c983e967fcb3f5fdaedfd6906da6e88cd
Kyle Miller <kylemiller995interviews@gmail.com>
2021-03-21 22:52:15 +0000

    Add Fake bank web api with POST route

---
+++ src/PaymentGateway.FakeBankApi/BankResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentGateway.FakeBankApi
+{
+    public class BankResponse
+    { 
+        public long? PaymentId { get; set; }
+        public string DetailedMessage { get; set; }
+        public string Status { get; set; }
+    }
+}
---
+++ src/PaymentGateway.FakeBankApi/Controllers/FakeBankController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentGateway.FakeBankApi.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class FakeBankController : ControllerBase
+    {
+
+        [HttpPost("ProcessPayment")]
+        public IActionResult ProcessPaymentAsync([FromBody] PaymentRequest paymentRequest)
+        {
+            if (paymentRequest.Amount == 100)
+            {
+                return Ok(new BankResponse
+                {
+                    PaymentId = 1000,
+                    DetailedMessage = "Payment has been processed successfully",
+                    Status = "Processed"
+                });
+            }
+            if (paymentRequest.Amount == 1000)
+            {
+                return BadRequest(new BankResponse
+                {
+                    DetailedMessage = "Payment has been processed successfully",
+                    Status = "Failed"
+                });
+            }
+            return BadRequest();
+        }
+    }
+}
