2182deccdcf2852f85947a9925e05e6ea1380554
Kyle Miller <kylemiller995interviews@gmail.com>
2021-03-22 03:58:34 +0000

    add failing integrations tests & runner to display auth works

---
+++ src/PaymentGateway.Gateway.IntegrationTests/Utilities/Fakes/FakeStartup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PaymentGateway.Gateway.IntegrationTests.Utilities.Fakes
+{
+    public class FakeStartup : Startup
+    {
+
+        public FakeStartup(IConfiguration configuration, IWebHostEnvironment webHostEnvironment) : base(configuration, webHostEnvironment)
+        {
+
+        }
+
+        protected override void ConfigureAuth(IServiceCollection services)
+        {
+
+        }
+    }
+}
