5e5212b5b9f93632f7853061afd89b156c924227
Kyle Miller <kylemiller995interviews@gmail.com>
2021-03-24 11:14:28 +0000

    Fix old integration tests to bypass auth. Add integration tests for auth

---
+++ src/PaymentGateway.Gateway.IntegrationTests/Utilities/Fakes/FakePolicyEvaluator.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Policy;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace PaymentGateway.Gateway.IntegrationTests.Utilities.Fakes
+{
+    public class FakePolicyEvaluator : IPolicyEvaluator
+    {
+        public virtual async Task<AuthenticateResult> AuthenticateAsync(AuthorizationPolicy policy, HttpContext context)
+        {
+            var testScheme = "FakeScheme";
+            var principal = new ClaimsPrincipal();
+            return await Task.FromResult(AuthenticateResult.Success(new AuthenticationTicket(principal,
+                new AuthenticationProperties(), testScheme)));
+        }
+
+        public virtual async Task<PolicyAuthorizationResult> AuthorizeAsync(AuthorizationPolicy policy,
+            AuthenticateResult authenticationResult, HttpContext context, object resource)
+        {
+            return await Task.FromResult(PolicyAuthorizationResult.Success());
+        }
+    }
+}
